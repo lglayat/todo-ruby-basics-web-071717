0b7b7f079f575ca79821a293a118948fe6475023
Luke Glayat <glayatle@g.cofc.edu>
2017-07-05 14:46:46 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+  quotient = num1/num2
+  return quotient
 end
 
 def assign_variable(value)
-  
+  varName = value
 end
 
-def argue
-  
+def argue(argument)
+  return argument
 end
 
-def greeting
+def greeting(arg1, arg2)
 
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+  return "expert"
 end
 
-def pizza_party
-
+def pizza_party(arg1 = "cheese")
+  return arg1
 end
